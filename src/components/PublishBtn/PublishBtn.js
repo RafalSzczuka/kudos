46e556a89feb40b11ae21e78baaae8de6e12fa67
RafalSzczuka <53195389+RafalSzczuka@users.noreply.github.com>
2020-05-26 19:19:50 +0200

    spinner loader added

--- src/components/PublishBtn/PublishBtn.js
+++ src/components/PublishBtn/PublishBtn.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import publishBtnStyle from "./publishBtn.module.scss";
 
 const PublishBtn = ({ valid, handler }) => {
   return (
     <button className={publishBtnStyle.publishBtn} onClick={handler}>
       {valid ? (
-        <Link to="/" style={{ color: "inherit", textDecoration: "inherit" }}>
-          Opublikuj
-        </Link>
+        <div
+          className="spinner-border"
+          role="status"
+          style={{ width: "25px", height: "25px" }}
+        ></div>
       ) : (
         "Opublikuj"
       )}
