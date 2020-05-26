bc1254b7490c078ae8a7d17fe084bf44aee96638
RafalSzczuka <53195389+RafalSzczuka@users.noreply.github.com>
2020-05-26 21:15:46 +0200

    Updates

---
+++ precache-manifest.2c749a95baa827e01452c28ec17d09a7.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a3e7dbaae82232b668d330ca3da511",
+    "url": "/kudos/index.html"
+  },
+  {
+    "revision": "6c75bc345ed2a10ee2df",
+    "url": "/kudos/static/css/main.a4d5d0b3.chunk.css"
+  },
+  {
+    "revision": "35000faa46ce1dfba1b8",
+    "url": "/kudos/static/js/2.d531e9bb.chunk.js"
+  },
+  {
+    "revision": "452512fa9f48054be807e37947522c62",
+    "url": "/kudos/static/js/2.d531e9bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c75bc345ed2a10ee2df",
+    "url": "/kudos/static/js/main.b1015ff1.chunk.js"
+  },
+  {
+    "revision": "93afc2fdf41430660c3d",
+    "url": "/kudos/static/js/runtime-main.217eacac.js"
+  },
+  {
+    "revision": "18722765ed22b684ba1855363b062f60",
+    "url": "/kudos/static/media/close.18722765.svg"
+  },
+  {
+    "revision": "54a311549f908f6974cd34471c6a7719",
+    "url": "/kudos/static/media/congrats.54a31154.svg"
+  },
+  {
+    "revision": "a4476b0c83dae78c51e9186371d2e4a5",
+    "url": "/kudos/static/media/coworker.a4476b0c.svg"
+  },
+  {
+    "revision": "067595ad77ecc0db9c81c8905a7eef32",
+    "url": "/kudos/static/media/fa-brands-400.067595ad.woff2"
+  },
+  {
+    "revision": "57dcda6f368ea90179f75cbdae96c263",
+    "url": "/kudos/static/media/fa-brands-400.57dcda6f.eot"
+  },
+  {
+    "revision": "9d67fa1429375bd2a899a17eb77d0342",
+    "url": "/kudos/static/media/fa-brands-400.9d67fa14.svg"
+  },
+  {
+    "revision": "9ec698d1a597bff5df337094b71ddaaf",
+    "url": "/kudos/static/media/fa-brands-400.9ec698d1.ttf"
+  },
+  {
+    "revision": "b564da88bbf0c4aa446fa19653713cd1",
+    "url": "/kudos/static/media/fa-brands-400.b564da88.woff"
+  },
+  {
+    "revision": "3351f435b3c9037fd88aeb04dc1e43bc",
+    "url": "/kudos/static/media/fa-regular-400.3351f435.eot"
+  },
+  {
+    "revision": "4165c2688309cbfb1b877caf8f75afb5",
+    "url": "/kudos/static/media/fa-regular-400.4165c268.woff2"
+  },
+  {
+    "revision": "5d0861781aeef6c82fda3a3076954a1b",
+    "url": "/kudos/static/media/fa-regular-400.5d086178.svg"
+  },
+  {
+    "revision": "73cf49a2232c06c920b7a34e36bfb58c",
+    "url": "/kudos/static/media/fa-regular-400.73cf49a2.woff"
+  },
+  {
+    "revision": "a0e3ac82940c1998c5977fd4bc1f5ef6",
+    "url": "/kudos/static/media/fa-regular-400.a0e3ac82.ttf"
+  },
+  {
+    "revision": "0724bb8b89ab6b8b9b7df917b17be0b7",
+    "url": "/kudos/static/media/fa-solid-900.0724bb8b.svg"
+  },
+  {
+    "revision": "55eb2a60e8181f0e68b558c991973bf0",
+    "url": "/kudos/static/media/fa-solid-900.55eb2a60.woff2"
+  },
+  {
+    "revision": "75f38a159982b6bd1704891332d95fa7",
+    "url": "/kudos/static/media/fa-solid-900.75f38a15.ttf"
+  },
+  {
+    "revision": "89e02bae13c9131c7468b1e729339ac1",
+    "url": "/kudos/static/media/fa-solid-900.89e02bae.eot"
+  },
+  {
+    "revision": "cdfec5cf5e9840889790bcf2c4042583",
+    "url": "/kudos/static/media/fa-solid-900.cdfec5cf.woff"
+  },
+  {
+    "revision": "c119efe0bbf5e5f5c314b8b654661c8c",
+    "url": "/kudos/static/media/hands.c119efe0.svg"
+  },
+  {
+    "revision": "c8feb7027bdf1725196d9ed65c1f7e13",
+    "url": "/kudos/static/media/outboundaries.c8feb702.svg"
+  },
+  {
+    "revision": "de9ee77271ef1ac605730f8a89f85258",
+    "url": "/kudos/static/media/positive.de9ee772.svg"
+  },
+  {
+    "revision": "5b7a641bdb30fd6cdef83f49712a55a8",
+    "url": "/kudos/static/media/teamplay.5b7a641b.svg"
+  },
+  {
+    "revision": "da5217bdc30fdd1ec38b6f3c2fa110da",
+    "url": "/kudos/static/media/thanks.da5217bd.svg"
+  }
+]);
